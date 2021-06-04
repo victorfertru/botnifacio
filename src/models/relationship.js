83650fccf80c2e576aebf924e03e1e240cf087f5
Vic <victor.fertru@gmail.com>
2021-06-04 21:39:02 +0200

    Add routes for users in server main file

--- src/models/relationship.js
+++ src/models/relationship.js
@@ -2,7 +2,9 @@ const dbConnection = require("../config/db");
 const User = require("./User");
 
 const loadModels = () => {
-  dbConnection.sync().then(() => console.log("All models loaded"));
+  dbConnection
+    .sync({ alter: true })
+    .then(() => console.log("All models loaded"));
 };
 
 module.exports = loadModels;
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const loadModels = require("./models/relationship");
 
 const indexRouter = require("./routes/index");
-//const usersRouter = require("./routes/userRouter");
+const usersRouter = require("./routes/users");
 
 const PORT = process.env.PORT || 8080;
 
@@ -18,5 +18,6 @@ server.use(express.static(path.join(__dirname, "public")));
 
 // @Routes
 server.use("/", indexRouter);
+server.use("/users", usersRouter);
 
 server.listen(PORT, () => console.log(`Everything is fine! in PORT: ${PORT}`));
